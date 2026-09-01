ec7a883bcc80225c45cfa16f06f9faacd6665674
Kalyani Yalla <yallakalyani2003@gmail.com>
2026-09-01 23:06:56 +0530

    git ignore file is added

---
+++ s3.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "kalyani-demo-bucket-630434208583"
+  force_destroy = true
+
+  tags = {
+    Name        = "Demo S3 Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
